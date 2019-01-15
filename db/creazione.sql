954a569683d64150a817d6ca6a4f0a5ab97159af
Marco Comparato <1826859+zzotta@users.noreply.github.com>
2019-01-15 21:48:10 +0100

    aggiunta tabella piloti

--- db/creazione.sql
+++ db/creazione.sql
@@ -1,15 +1,18 @@
-create table `campionati` (
-  `nome` text not null,
+drop table if exists gare;
+drop table if exists campionati;
+
+create table if not exists `campionati` (
+  `nome` text not null collate nocase,
   `anno` integer not null,
   primary key(nome, anno)
 );
 
-create table `gare` (
-  `nome` text not null,
-  `anno` integer not null,
+create table if not exists `gare` (
+  `nome` text collate nocase,
+  `anno` integer,
   `numero` integer not null,
-  `luogo` text not null,
+  `luogo` text not null collate nocase,
   `data` text not null,
-  primary key(nome, anno, numero)
-  foreign key(nome, anno) references campionato(nome, anno)
+  primary key(nome, anno, numero),
+  foreign key(nome, anno) references campionati(nome, anno) on delete set null on update cascade
 );
--- db/inserimenti.fake.sql
+++ db/inserimenti.fake.sql
@@ -1,3 +1,14 @@
 insert into campionati values('rocks inside champioship', 2018);
 insert into campionati values('rocks inside champioship', 2019);
 insert into campionati values('sorrca', 2019);
+
+insert into gare values('rocks inside champioship', 2018, 1, 'casalecchio', '15/01/2019');
+insert into gare values('rocks inside champioship', 2018, 2, 'casalecchio', '15/01/2019');
+insert into gare values('rocks inside champioship', 2018, 3, 'casalecchio', '15/01/2019');
+insert into gare values('rocks inside champioship', 2019, 1, 'casalecchio', '15/01/2019');
+
+insert into piloti values('massimo', 'arcara');
+insert into piloti values('marco', 'comparato');
+
+-- delete from campionati where anno = 2018;
+-- delete from gare where nome is null;
