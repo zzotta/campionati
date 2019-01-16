229266b013e4c2d1188e1d29ac82a1bd2211fd6c
Marco Comparato <1826859+zzotta@users.noreply.github.com>
2019-01-16 16:54:30 +0100

    wip: rinomino alcune colonne

--- db/creazione.sql
+++ db/creazione.sql
@@ -28,9 +28,25 @@ create table if not exists piloti (
 create table if not exists partecipazioni (
   pilota_nome text not null collate nocase,
   pilota_cognome text not null collate nocase,
-  gara_nome text not null collate nocase,
-  gara_anno integer not null,
+  campionato_nome text not null collate nocase,
+  campionato_anno integer not null,
   gara_numero integer not null,
+  primary key(pilota_nome, pilota_cognome, campionato_nome, campionato_anno, gara_numero),
+  foreign key(pilota_nome, pilota_cognome) references piloti(nome, cognome) on update cascade,
+  foreign key(campionato_nome, campionato_anno, gara_numero) references gare(campionato_nome, campionato_anno, numero) on update cascade
+);
+
+create table if not exists manches (
+  pilota_nome text not null collate nocase,
+  pilota_cognome text not null collate nocase,
+  campionato_nome text not null collate nocase,
+  campionato_anno integer not null,
+  gara_numero integer not null,
+  categoria text not null collate nocase,
+  numero interger not null,
+  penalita integer not null,
+  verricello integer not null,
+  tempo integer not null,
   primary key(pilota_nome, pilota_cognome, gara_nome, gara_anno, gara_numero),
   foreign key(pilota_nome, pilota_cognome) references piloti(nome, cognome) on update cascade,
   foreign key(gara_nome, gara_anno, gara_numero) references gare(nome, anno, numero) on update cascade
