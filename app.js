64fd735858d52ca24b05cd0f38c378972e19f481
Marco Comparato <1826859+zzotta@users.noreply.github.com>
2019-01-18 15:58:47 +0100

    autenticazione

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const Sequelize = require('sequelize');
+const path = require('path');
 
 const sequelize = new Sequelize(null, null, null, {
   operatorsAliases: false,
   dialect: 'sqlite',
-  storage: '/home/marlin/Documents/dev/node/projects/campionati/db/campionati.db'
+  storage: path.join(__dirname, 'db/campionati.db')
 });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+  
