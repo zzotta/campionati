5626ab355b4c7157a578e125ec9c2dfb2c0aaaa6
Marco Comparato <1826859+zzotta@users.noreply.github.com>
2019-05-12 21:34:41 +0200

    remove switch and tab navigator

--- expo/tabs/navigation/AppNavigator.js
+++ expo/tabs/navigation/AppNavigator.js
@@ -1,9 +1,7 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+import HomeScreen from '../screens/HomeScreen';
 
-export default createAppContainer(createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+export default createAppContainer(createStackNavigator({
+  Home: HomeScreen,
 }));
